0078c40e54fc7fe6f3730f6d718aac3b488c51dc
“Kateryna <yatskivkath@gmail.com>
2024-02-24 22:00:14 +0100

    HW-2 added app_error.log logging for errors

--- homework-2/lib/logger/appenders/file.js
+++ homework-2/lib/logger/appenders/file.js
@@ -1,11 +1,18 @@
 import formaters from "../utils/formatMessage.js";
+import * as constants from "../constants.js";
 import fs from "fs";
+
+const path = process.env.LOG_FILE ?? "app.log";
+const error_path = path.substring(0, path.lastIndexOf('.')) + '_error' + path.substring(path.lastIndexOf('.'));  
 
 function log(date, level, category, message) {
     const text = formaters.formatMessage(date, level, category, message) + '\n';
-    const path = process.env.LOG_FILE ?? "app.log";
+    
+    fs.appendFile(path, text, {} , () => {});
 
-    fs.appendFile(path, text, {} , () => {})
+    if(level === constants.level.ERROR) {
+        fs.appendFile(error_path, text, {}, () => {});
+    }
 }
 
 export default {log}
