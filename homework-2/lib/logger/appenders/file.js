47653b0d2e020cec697e4b6d7eddb348c3cf55c3
“Kateryna <yatskivkath@gmail.com>
2024-02-28 22:48:19 +0100

    HW-3 updated fileAppender to be able to parse CSV

--- homework-2/lib/logger/appenders/file.js
+++ homework-2/lib/logger/appenders/file.js
@@ -2,20 +2,57 @@ import formaters from "../utils/formatMessage.js";
 import * as constants from "../constants.js";
 import fs from "fs";
 
-const path = process.env.LOG_FILE ?? "app.log";
-const error_path = path.substring(0, path.lastIndexOf('.')) + '_error' + path.substring(path.lastIndexOf('.'));  
+const CSV_HEADER = "Date;Level;Category;Message\n"
 
-function log(date, level, category, message, format) {
-    const text = formaters.formatMessage(date, level, category, message, format) + '\n';
+function logDefault(timestamp, level, category, message, format) {
+    const path = process.env.LOG_FILE ?? "app.log";
+    const error_path = path.substring(0, path.lastIndexOf('.')) + '_error' + path.substring(path.lastIndexOf('.'));  
+    const text = formaters.formatMessage(timestamp, level, category, message, format) + '\n';
     
     fs.appendFile(path, text, {} , (err) => {
         if(err) throw err;
     });
 
-    if(level === constants.level.ERROR) {
+    if (level === constants.level.ERROR) {
         fs.appendFile(error_path, text, {}, (err) => {
             if(err) throw err;
         });
+    }
+}
+
+function logCsv(timestamp, level, category, message, format) {
+    const date = new Date(timestamp);
+    const path = `app_${date.getDate()}_${date.getMonth()}_${date.getFullYear()}.log.csv`;
+    const error_path = `app_error_${date.getDate()}_${date.getMonth()}_${date.getFullYear()}.log.csv`;
+    const text = formaters.formatMessage(date, level, category, message, format) + '\n';
+
+    if(!fs.existsSync(path)) {
+        fs.writeFileSync(path, CSV_HEADER);
+    }
+
+    if(!fs.existsSync(error_path)) {
+        fs.writeFileSync(error_path, CSV_HEADER);
+    }
+
+    fs.appendFile(path, text, {} , (err) => {
+        if(err) throw err;
+    });
+
+    if (level === constants.level.ERROR) {
+        fs.appendFile(error_path, text, {}, (err) => {
+            if(err) throw err;
+        });
+    }
+}
+
+function log(timestamp, level, category, message, format) {
+    switch (format) {
+        case constants.format.CSV: {
+            return logCsv(timestamp, level, category, message, format);
+        }
+        default: {
+            return logDefault(timestamp, level, category, message, format);
+        }
     }
 }
 
