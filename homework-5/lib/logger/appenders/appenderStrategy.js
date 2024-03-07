7de80b43a74f763cf70ab28333b7104765149b56
“Kateryna <yatskivkath@gmail.com>
2024-03-07 09:00:07 +0100

    HW-5 added basic structure for networkAppender

--- homework-5/lib/logger/appenders/appenderStrategy.js
+++ homework-5/lib/logger/appenders/appenderStrategy.js
@@ -2,10 +2,12 @@ import * as constants from "../constants.js";
 import config from "../config/config.js";
 import consoleAppender from "./console.js"
 import fileAppender from "./file.js"
+import networkAppender from "./network.js";
 
 const appenderFunctions = {
     [constants.appender.CONSOLE]: consoleAppender,
-    [constants.appender.FILE]: fileAppender
+    [constants.appender.FILE]: fileAppender,
+    [constants.appender.NETWORK]: networkAppender,
 }
 
 function getAppenders() {
---
+++ homework-5/lib/logger/appenders/network.js
@@ -0,0 +1,5 @@
+function listen() {
+
+}
+
+export {listen}
