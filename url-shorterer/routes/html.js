ea67383acc13d9b40c14e6f917d099d1b89a6e15
“Kateryna <yatskivkath@gmail.com>
2024-03-25 11:26:12 +0100

    HW-9 renamed middleware files

--- url-shorterer/routes/html.js
+++ url-shorterer/routes/html.js
@@ -1,5 +1,5 @@
 import express from "express";
-import auth from "../middlewares/auth.js";
+import auth from "../middlewares/authMiddleware.js";
 import { getUrlsView, getUsersView } from "../controllers/htmlController.js";
 
 const router = new express.Router();
